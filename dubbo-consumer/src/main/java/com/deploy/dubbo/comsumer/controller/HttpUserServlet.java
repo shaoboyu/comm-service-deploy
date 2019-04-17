4ce9862c377213032b86e2d5a552cdc9d014653d
yushaobo <yusahobo@feinno.com>
2019-04-17 15:51:41 +0800

    修复依赖

--- dubbo-consumer/src/main/java/com/deploy/dubbo/comsumer/controller/HttpUserServlet.java
+++ dubbo-consumer/src/main/java/com/deploy/dubbo/comsumer/controller/HttpUserServlet.java
@@ -1,5 +1,6 @@
 package com.deploy.dubbo.comsumer.controller;
 
+import com.demo.dubbo.User;
 import com.deploy.dubbo.comsumer.service.CityDubboConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
--- undertow-demo/src/main/java/com/undertow/servlet/dubbo/provider/UserServiceImpl.java
+++ undertow-demo/src/main/java/com/undertow/servlet/dubbo/provider/UserServiceImpl.java
@@ -1,18 +1,18 @@
-package com.undertow.servlet.dubbo.provider;
-
-import com.alibaba.dubbo.config.annotation.Service;
-import com.demo.dubbo.User;
-import com.demo.dubbo.UserService;
-
-/**
- * @author: shaoboyu@hotmail.com
- * @create: 19-4-17
- **/
-@Service(interfaceClass = UserServiceImpl.class)
-public class UserServiceImpl implements UserService {
-    @Override
-    public User saveUser(User user) {
-        System.out.println("user is :"+user.toString());
-        return null;
-    }
-}
+//package com.undertow.servlet.dubbo.provider;
+//
+//import com.alibaba.dubbo.config.annotation.Service;
+//import com.demo.dubbo.User;
+//import com.demo.dubbo.UserService;
+//
+///**
+// * @author: shaoboyu@hotmail.com
+// * @create: 19-4-17
+// **/
+//@Service(interfaceClass = UserServiceImpl.class)
+//public class UserServiceImpl implements UserService {
+//    @Override
+//    public User saveUser(User user) {
+//        System.out.println("user is :"+user.toString());
+//        return null;
+//    }
+//}
